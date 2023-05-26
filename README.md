18e13ef872353ae4e51826495d3463ee39dbd315
RaheimDevins <raheimdev2011@gmail.com>
2023-05-25 21:52:47 -0500

    Added functional content to files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Raheims-Weather-App
+
+## Description
+This page is a weather app created using Front-End devloper skills such as HTML, CSS, JavaScript. also with new skills using 3rd party APIs 
+
+## Installation
+
+N/A
+
+## Usage
+
+This page is used to check the weather of any city, along with the future forcast for the following 5 days.
+
+## Deployment
+
+
+
+
+## Credits
+
+N/A
+
+## License
+
+Please refer to LICENSE in repo.
--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,74 @@
-var weather = {
-  apiKey: 'de1bbc386e39ebaf5f3331ab62cf451e',
-  fetchWeather: function (city) {
-    fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q=city&units=imperial&appid=de1bbc386e39ebaf5f3331ab62cf451e'
-    ).then((Response) => Response.json())
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-const { name } = data;
-const { temp, humidity } = data.main;
-const { wind } = data.wind;
-console.log(name,temp,humidity,wind)
-document.querySelector('#city').innerText = name;
-document.querySelector('#cTemp').innerText = temp + 'degrees symbolF';
-document.querySelector('#cWind').innerText = wind;
-document.querySelector('#cHumid').innerText = humidity;
-  }
+
+const apiKey = 'de1bbc386e39ebaf5f3331ab62cf451e';
+const fiveDaySectionEl = document.querySelector('#fivedays');
+
+function displayWeather(data) {
+  console.log(data)
+  const { name } = data;
+  const { temp, humidity } = data.main;
+  const { wind } = data
+  console.log(name, temp, humidity, wind)
+  document.querySelector('#city').innerText = 'city-' + name;
+  document.querySelector('#cTemp').innerText = 'Temp-' + temp + '°F';
+  document.querySelector('#cWind').innerText = 'Wind-' + wind.speed + 'MPH';
+  document.querySelector('#cHumid').innerText = 'Humidity-' + humidity + '%';
+}
 
 
+//-- 5 day weather fetch --//
+function fetchFiveDayWeather(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+  ).then((Response) => Response.json())
+    .then((data) => displayWeather(data));
+  fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+  ).then((Response) => Response.json())
+    .then((data) => displayFiveDayWeather(data.list));
 
 };
+
+function displayFiveDayWeather(weatherList) {
+  for (var i = 4; i < weatherList.length; i += 8) {
+    const dayWeather = weatherList[i];
+    const date = dayjs(dayWeather.dt_txt).format('M/D/YYYY');
+    const { temp, humidity } = dayWeather.main;
+    const { speed } = dayWeather.wind;
+
+    const cardEl = document.createElement('div');
+    const ulEl = document.createElement('ul');
+    cardEl.append(ulEl);
+    const dateLi = document.createElement('li');
+    dateLi.textContent = date;
+    const tempLi = document.createElement('li');
+    tempLi.textContent = temp +"°F";
+    const windLi = document.createElement('li');
+    windLi.textContent = speed + 'MPH';
+    const humidLi = document.createElement('li');
+    humidLi.textContent = humidity + '%';
+    ulEl.append(dateLi, tempLi, windLi, humidLi);
+
+    fiveDaySectionEl.append(cardEl);
+
+  }
+
+}
+
+
+
+
+
+
+
+
+
+document.querySelector("#s-Btn").addEventListener('click', function () {
+  const city = document.querySelector("#search-txt").value;
+  //fetchWeather(city)
+  fetchFiveDayWeather(city);
+});
+
+
 
 
 
